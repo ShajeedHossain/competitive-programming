3e4252e77e5c1cc9fc2a88653eaf04d0e698ef44
ShajeedHossain <shakun650@gmail.com>
2026-01-21 13:59:37 +0600

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, target;
+        cin >> n >> target;
+        // string s;
+        // getline(cin, s);
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+    }
+}
